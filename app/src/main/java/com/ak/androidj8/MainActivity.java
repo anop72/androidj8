a9a0b02a06fd6839fe142ce56c9d4b0cec35d59c
ak <anop72@gmail.com>
2016-10-08 01:08:05 +0700

    init

---
+++ app/src/main/java/com/ak/androidj8/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ak.androidj8;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
